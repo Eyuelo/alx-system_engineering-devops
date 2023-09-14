167c3b7e7f3d3d6c9c51d934b5f1bba2c0b19da6
Eyuel <eyuelnegash21@gmail.com>
2023-09-13 19:27:19 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,3 +1,10 @@
+#include "main.h"
+/**
+ * main - print _putchar
+ * is written by mbah
+ * Return: 0
+ */
+
 int main(void)
 {
 _putchar('_');
@@ -11,3 +18,4 @@ _putchar('r');
 _putchar('\n');
 return (0);
 }
+
