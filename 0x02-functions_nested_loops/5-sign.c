2b1719eb2dc32527f4dfa15904a11f3bb92ae2d9
Eyuel <eyuelnegash21@gmail.com>
2023-09-20 05:55:44 -0700

    Task 5. Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,19 +7,20 @@
  */
 int print_sign(int n)
 {
+
 	if (n > 0)
 	{
-	_putchar(20);
+	_putchar(43);
 	return (1);
 	}
 	else if (n < 0)
 	{
-	_putchar(20);
+	_putchar(45);
 	return (-1);
 	}
 	else
 	{
-	_putchar(15);
+	_putchar(45);
 	return (0);
 	}
 }
